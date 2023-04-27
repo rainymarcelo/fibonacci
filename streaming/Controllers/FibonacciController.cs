1dc1d903c43dd81ff1e32439df32b950b8df6e79
rainymarcelo <marcelo.delahozs@udea.edu.co>
2023-04-27 15:13:31 -0500

    Initial commit

---
+++ streaming/Controllers/FibonacciController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace streaming.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FibonacciController : ControllerBase
+    {
+        [HttpGet("{numero}")]
+        public double FibonacciRecursivo(int numero)
+        {
+            return
+            Enumerable.Range(1, numero)
+                     .Skip(2)
+                     .Aggregate(new {  Current = 1.0, Prev = 1.0 },
+                                (x, index) => new { Current = x.Prev + x.Current, Prev = x.Current })
+                     .Current;
+        }
+    }
+}
